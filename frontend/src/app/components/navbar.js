d14df1b1558eebfa2453cb23c12b4dae9d2c7f04
Your Name <you@example.com>
2024-08-24 22:31:32 +0530

    added codes]

---
+++ frontend/src/app/components/navbar.js
@@ -0,0 +1,11 @@
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,11 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import Navbar from "./components/navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Navbar/>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
